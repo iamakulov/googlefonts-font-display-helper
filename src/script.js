0f454ea26bc2f19bf7c21b1c96b7eada176991db
Jacob Groß <kurtextrem@gmail.com>
2019-02-15 14:18:35 +0100

    Code golfing

--- src/script.js
+++ src/script.js
@@ -1,26 +1,32 @@
 (function() {
+  'use strict';
+  
   // $FONT_STYLESHEET$ and $FONT_DISPLAY$ values are replaced by the snippet generator
   // when the snippet is generated
   var fontStylesheet = '%FONT_STYLESHEET%';
   var fontDisplayValue = '%FONT_DISPLAY%';
   var cssLocalStorageKey = '__3perf_googleFontsStylesheet';
-
+  
+  function append(el) {
+    (document.head || document.body).appendChild(el);
+  }
+  
   function insertFallback() {
     var link = document.createElement('link');
     link.href = fontStylesheet;
     link.rel = 'stylesheet';
-    (document.head || document.body).appendChild(link);
+    append(link);
   }
 
   function patchAndInsertStylesheet(stylesheet) {
     var stylesheetWithFontDisplay = stylesheet.replace(
       /@font-face {/g,
-      '@font-face {\n  font-display: ' + fontDisplayValue + ';'
+      '@font-face{font-display:' + fontDisplayValue + ';'
     );
 
     var style = document.createElement('style');
     style.innerHTML = stylesheetWithFontDisplay;
-    (document.head || document.body).appendChild(style);
+    append(style);
   }
 
   var isFontDisplaySupported =
@@ -30,8 +36,8 @@
     return;
   }
 
-  if (localStorage.getItem(cssLocalStorageKey)) {
-    var stylesheet = localStorage.getItem(cssLocalStorageKey);
+  if (localStorage[cssLocalStorageKey]) {
+    var stylesheet = localStorage[cssLocalStorageKey];
     patchAndInsertStylesheet(stylesheet);
     // Still initiate fetch() to avoid “Unused <link rel="preload">” warnings
     fetch(fontStylesheet).then(function() {});
@@ -43,7 +49,7 @@
       return response.text();
     })
     .then(function(stylesheet) {
-      localStorage.setItem(cssLocalStorageKey, stylesheet);
+      localStorage[cssLocalStorageKey] = stylesheet;
       return stylesheet;
     })
     .then(patchAndInsertStylesheet)
